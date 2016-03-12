222ac62b46ccb177f793d2a8e608a9698941c374
morales.saldarriaga <morales.saldarriaga@gmail.com>
2016-03-11 19:05:00 -0500

    update mailer view

--- app/mailers/suscription_mailer.rb
+++ app/mailers/suscription_mailer.rb
@@ -3,6 +3,6 @@ class SuscriptionMailer < ApplicationMailer
   def notification_email(suscriptor)
     @user = suscriptor
     @url  = 'http://example.com/login'
-    mail(from: suscriptor.email, to: Suscription::EMAIL_OWNER, subject: 'consulta de la web')
+    mail(from: suscriptor.email, to: Suscription::EMAIL_OWNER, subject: 'KMGOLD CONTACTO')
   end
 end
