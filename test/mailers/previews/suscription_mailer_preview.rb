8052d568783fa66956ec5afba25ed66421c136d2
Jorge Crisóstomo Palacios <videmor@gmail.com>
2016-03-24 21:07:31 -0500

    Add test mailer

--- test/mailers/previews/suscription_mailer_preview.rb
+++ test/mailers/previews/suscription_mailer_preview.rb
@@ -1,4 +1,7 @@
 # Preview all emails at http://localhost:3000/rails/mailers/suscription_mailer
 class SuscriptionMailerPreview < ActionMailer::Preview
 
+  def notification_email_preview
+		SuscriptionMailer.notification_email(Suscription.first).deliver
+  end
 end
